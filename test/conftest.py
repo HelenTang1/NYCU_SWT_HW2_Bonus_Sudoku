46cfe6d3466e7d53eb8526d87fc585da171f1b26
Helentang1 <yuijw0720@gmail.com>
2025-10-23 21:39:44 +0800

    refactor, add(test): add the test_find_empties  and refactor the valid fixture into conftest

---
+++ test/conftest.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append(".")
+import pytest
+import numpy as np
+from sudoku import Grid
+
+@pytest.fixture()
+def a_valid_grid() -> Grid:
+    return Grid(np.array([
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]), unknown=0)
--- test/test_grid.py
+++ test/test_grid.py
@@ -4,20 +4,6 @@ import pytest
 import numpy as np
 from sudoku import Grid
 
-@pytest.fixture()
-def a_valid_grid() -> Grid:
-    return Grid(np.array([
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
-    ]), unknown=0)
-
 def test_isValidGrid_valid(a_valid_grid):
     assert a_valid_grid.isValidGrid() == True
 
@@ -85,3 +71,17 @@ def test_isValidGrid_invalid(grid_data):
 )
 def test_isValidMove(a_valid_grid, row, col, value, expected):
     assert a_valid_grid.isValidMove(row, col, value) == expected
+
+def test_find_empties(a_valid_grid):
+    empty_cells = a_valid_grid.find_empties()
+    assert empty_cells == [
+        (0, 2), (0, 3), (0, 5), (0, 6), (0, 7), (0, 8),
+        (1, 1), (1, 2), (1, 6), (1, 7), (1, 8),
+        (2, 0), (2, 3), (2, 4), (2, 5), (2, 6), (2, 8),
+        (3, 1), (3, 2), (3, 3), (3, 5), (3, 6), (3, 7),
+        (4, 1), (4, 2), (4, 4), (4, 6), (4, 7),
+        (5, 1), (5, 2), (5, 3), (5, 5), (5, 6), (5, 7),
+        (6, 0), (6, 2), (6, 3), (6, 4), (6, 5), (6, 8),
+        (7, 0), (7, 1), (7, 2), (7, 6), (7, 7),
+        (8, 0), (8, 1), (8, 2), (8, 3), (8, 5), (8, 6)
+    ]
