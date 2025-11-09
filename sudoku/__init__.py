416bfe30797fe25815c383d407e6b44ecaeaa8aa
Helentang1 <yuijw0720@gmail.com>
2025-11-09 22:22:05 +0800

    style: change the style of export __all__ in __init__

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -2,4 +2,6 @@ from .grid import Grid
 from .solver import Solver, HumanLogicSolver
 from .gen import Generator
 from .rating import Rank, get_rating
-all = [Grid, Solver, Generator, Rank, HumanLogicSolver, get_rating,]
+
+__all__ = ["Grid", "Solver", "Generator", 
+           "Rank", "HumanLogicSolver", "get_rating"]
